8bba2a9a0d675033a543449fc9069de7b87d68cc
Vinu34-del <vd3711211@gmail.com>
2025-10-15 20:21:04 +0530

    First commit

---
+++ Variables and Data Types-2/Type Conversion/1.py
@@ -0,0 +1,8 @@
+age = 20
+print(age)
+
+age_float = float(age)
+print(age_float)  #type conversion from int to float
+
+s = "100"
+print(int(s)+age)  #another example of type conversion from str to int
