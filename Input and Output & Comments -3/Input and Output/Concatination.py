78d6b95dce2292af8cc0dec63c8121e8f138911a
Vinu34-del <vd3711211@gmail.com>
2025-10-15 22:37:24 +0530

    First commit

---
+++ Input and Output & Comments -3/Input and Output/Concatination.py
@@ -0,0 +1,11 @@
+boy_name = input("Boy Name: ")
+boy_age = int(input("Boy Age:"))
+girl_name = input("Girl Name: ")
+girl_age =int(input("Girl Age:"))
+age_diff = boy_age - girl_age
+print(boy_name + "Loves " +girl_name + "Age Diffrence is:" + str(age_diff))  #Concatination Method 1
+
+
+
+print(f" {boy_name} Loves {girl_name}. Age Differene is: {age_diff} ") #Concatination Method 2 (f-string)
+
