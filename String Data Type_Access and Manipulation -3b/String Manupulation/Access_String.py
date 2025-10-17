f3d629cbbffe9ef57065c73fc3c46016d1085242
Vinu34-del <vd3711211@gmail.com>
2025-10-17 20:09:02 +0530

    First Commit

---
+++ String Data Type_Access and Manipulation -3b/String Manupulation/Access_String.py
@@ -0,0 +1,8 @@
+name = "Vinay"
+print(name[2])          # Output: n  {indexing}
+
+print(name[1:5])        # Output: inay  {slicing}
+print(name[:5])
+print(name[1:])
+print(name[::])         # Output: Vinay {Accessing All}
+print(name[::2])        #Output : Vny   {Skip 2 letters}
---
+++ String Data Type_Access and Manipulation -3b/String Manupulation/Escape_Sequence.py
@@ -0,0 +1,5 @@
+S = "Vinay is a \n Good Boy."
+print(S)          # Output: Vinay is a and second line print good boy.
+
+v = "Hello \t Python."
+print(v)         # Output: Hello    Python. (tab space between hello and python)
