1c24b5f77218e5227375747866358a6294f913c3
Vinu34-del <vd3711211@gmail.com>
2025-10-18 22:12:10 +0530

    First Commit

---
+++ Operators in Python/Assignment_op.py
@@ -0,0 +1,10 @@
+a = 100
+a += 100
+print(a)   # Output: 200 , a = a + 100
+
+x = 10
+x -= 100
+print(x)  # Output: -90 , x = x - 100
+
+x *= 10
+print(x)  # Output: -900 , x = x * 10
---
+++ Operators in Python/Logical_op.py
@@ -0,0 +1,7 @@
+print(True and True)  #True
+
+print(True or False)  #True
+
+print(False and True)  #False
+
+print(not(True))    #false
