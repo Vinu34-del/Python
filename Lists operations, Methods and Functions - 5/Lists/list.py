16f5233ed2f534e865ef40b60bbc842717e2e665
Vinu34-del <vd3711211@gmail.com>
2025-10-21 22:42:13 +0530

    First Commit

---
+++ Lists operations, Methods and Functions - 5/Lists/list.py
@@ -0,0 +1,7 @@
+items = ["Bru","Sugar","Rise"]
+print(items)    
+
+print(items[0])
+
+l = ["bru",2,True]
+print(l)
---
+++ Lists operations, Methods and Functions - 5/Lists/operation.py
@@ -0,0 +1,16 @@
+items = ["Bru","Sugar","Rise"]
+print(items)
+items.pop(0)
+print(items)
+
+items.append("Good Day Buskit") #add new item
+print(items)
+
+items.remove("Sugar") #remove item
+print(items)
+
+items.insert(1,"milk") #insert item at index
+print(items)
+
+items[0] = "Normal CopyPowder" #change item at index
+print(items)
