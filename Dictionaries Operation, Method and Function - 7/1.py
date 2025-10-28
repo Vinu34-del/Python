5790ac41e29875fd090311143e251d3237f9ca77
Vinu34-del <vd3711211@gmail.com>
2025-10-28 15:20:38 +0530

    First Commit

---
+++ Dictionaries Operation, Method and Function - 7/1.py
@@ -0,0 +1,22 @@
+#Dictionary
+birthday = {
+    "Vinay" : "4-july-2005",
+    "Virat" : "5-nov-1988",
+    "Rohit" : "30-april-1987"
+}
+print(birthday)
+
+#Accessing Items
+print(birthday["Virat"]) #Output: 5-nov-1988
+print(birthday["Vinay"]) #Output: 4-july-2005
+print(birthday["Rohit"]) #Output: 30-april-1987
+
+#Adding Dictionary Items
+birthday["Rana"] = "2-dec-1990" 
+print(birthday)
+
+# Updating Dictionary Items
+birthday["Vinay"] = "3-dec-2000"
+print(birthday)
+
+#Removing Element
